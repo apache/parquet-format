affad3d909f832af8881b32a39e398e129acd4d0
Ryan Blue <blue@apache.org>
2017-10-06 17:18:15 -0700

    PARQUET-1124: Add lz4 and zstd compression codecs.

--- src/main/thrift/parquet.thrift
+++ src/main/thrift/parquet.thrift
@@ -348,7 +348,9 @@ enum CompressionCodec {
   SNAPPY = 1;
   GZIP = 2;
   LZO = 3;
-  BROTLI = 4;
+  BROTLI = 4; // Added in 2.3.2
+  LZ4 = 5;    // Added in 2.3.2
+  ZSTD = 6;   // Added in 2.3.2
 }
 
 enum PageType {
