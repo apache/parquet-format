658b3dece19771438158cdc770a4b49a800397fa
Gidon Gershinsky <gidon@il.ibm.com>
2018-06-14 16:48:49 +0300

    protect column crypto metadata

--- src/main/thrift/parquet.thrift
+++ src/main/thrift/parquet.thrift
@@ -660,6 +660,22 @@ struct ColumnMetaData {
   13: optional list<PageEncodingStats> encoding_stats;
 }
 
+struct ColumnCryptoMetaData {
+  /** Path in schema **/
+  1: required list<string> path_in_schema
+  
+  /** Encrypted or plaintext **/
+  2: required bool encrypted
+  
+  /** Column can be encrypted with the footer key,
+   * or a column-specific key **/
+  3: required bool encrypted_with_footer_key
+  
+  /** Metadata of the column-specific key.
+   * Ignored if encrypted_with_footer_key is true **/
+  4: optional binary column_key_metadata
+}
+
 struct ColumnChunk {
   /** File where column data is stored.  If not set, assumed to be same file as
     * metadata.  This path is relative to the current file.
@@ -687,8 +703,8 @@ struct ColumnChunk {
   /** Size of ColumnChunk's ColumnIndex, in bytes **/
   7: optional i32 column_index_length
   
-  /** Path in schema - for encrypted columns **/
-  8: optional list<string> path_in_schema
+  /** Column-specific crypto metadata (for non-uniform encryption only) **/
+  8: optional ColumnCryptoMetaData crypto_meta_data
 }
 
 struct RowGroup {
@@ -880,22 +896,6 @@ struct FileMetaData {
   7: optional list<ColumnOrder> column_orders;
 }
 
-struct ColumnCryptoMetaData {
-  /** Path in schema **/
-  1: required list<string> path_in_schema
-  
-  /** Encrypted or plaintext **/
-  2: required bool encrypted
-  
-  /** Column can be encrypted with the footer key,
-   * or a column-specific key **/
-  3: required bool encrypted_with_footer_key
-  
-  /** Metadata of the column-specific key.
-   * Ignored if encrypted_with_footer_key is true **/
-  4: optional binary column_key_metadata
-}
-
 /**
  * Supported encryption algorithms.
  */
@@ -922,14 +922,5 @@ struct FileCryptoMetaData {
    *  and variable parts (random or counter), keep the fixed
    *  part here **/
   5: optional binary iv_prefix
-
-  /** File uniform encryption: true if all columns, 
-   * and footer, are encrypted with the same key **/
-  6: required bool uniform_encryption
-  
-  /** Written only if uniform_encryption is false **/
-  7: optional list<ColumnCryptoMetaData> column_crypto_meta_data
 }
-
-
 
