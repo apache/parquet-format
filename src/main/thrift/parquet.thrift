38cd45b591a777a7e12e2dabc1aa655cb81193e6
Gidon Gershinsky <gg5070@gmail.com>
2020-07-29 16:38:14 +0300

    apply the comment change

--- src/main/thrift/parquet.thrift
+++ src/main/thrift/parquet.thrift
@@ -646,6 +646,8 @@ struct PageHeader {
    *     uncompressed definition levels and the compressed column values.
    *     If no compression scheme is specified, the CRC shall be calculated on
    *     the uncompressed concatenation.
+   * - In encrypted columns, CRC is calculated after page encryption; the
+   *   encryption itself is performed after page compression (if compressed)
    * If enabled, this allows for disabling checksumming in HDFS if only a few
    * pages need to be read.
    **/
