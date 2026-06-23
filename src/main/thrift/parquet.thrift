e8e3252c5d33139157c607550a3e06071d324c19
Divjot Arora <div.arora@databricks.com>
2026-06-23 13:16:31 +0000

    move int96 layout

--- src/main/thrift/parquet.thrift
+++ src/main/thrift/parquet.thrift
@@ -28,6 +28,13 @@ namespace java org.apache.parquet.format
  * with the encodings to control the on disk storage format.
  * For example INT16 is not included as a type since a good encoding of INT32
  * would handle this.
+ *
+ * The INT96 type is used only for timestamps and is laid out as follows:
+ * - The lower 4 bytes are little-endian signed 32-bit integer containing the Julian day: the
+ *   number of days since the Julian calendar epoch, where Julian day 2440588 corresponds to
+ *   1 January 1970 (the Unix epoch).
+ * - The upper 8 bytes are little-endian signed 64-bit integer containing the number of nanoseconds
+ *   elapsed since the second of the day.
  */
 enum Type {
   BOOLEAN = 0;
@@ -1202,12 +1209,6 @@ union ColumnOrder {
   /*
    * The INT96 timestamp type is ordered chronologically. Only columns of
    * physical type INT96 may use this ordering.
-   *
-   * When writing statistics for columns with INT96_TIMESTAMP_ORDER, two values
-   * must be compared as follows:
-   * - Compare the last 4 bytes (days) as a little-endian 32-bit signed integer
-   * - If equal last 4 bytes, compare the first 8 bytes (nanos) as a
-   *   little-endian 64-bit signed integer.
    */
   3: Int96TimestampOrder INT96_TIMESTAMP_ORDER;
 }
